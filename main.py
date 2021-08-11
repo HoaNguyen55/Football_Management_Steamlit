7273e3e77bd5618464ee0959ed2a75a4f99ba456
josephnguyen <josephnguyenle2310@gmail.com>
2021-08-12 02:02:54 +0700

    Fix issue and update searching engine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import base64
 
-import SessionState
 import pandas as pd
 import numpy as np
 import sqlite3
@@ -8,6 +7,7 @@ from sqlite3 import Connection
 import streamlit as st
 import datetime
 import os.path
+import copy as cp
 
 
 def init_db(conn: Connection):
@@ -51,11 +51,13 @@ class main:
         self.pos = ["Thủ Môn", "Hậu Vệ", 'Tiền Vệ', 'Tiền Đạo']
         self.club = ['Việt Nam', 'Nhật Bản', 'Saudi Arabia', 'Trung Quốc', 'Australia', 'Oman']
         self.removeOpt = ('Xóa tất cả', 'Xóa từng dòng')
-        self.qaOpt = ('Tất cả cầu thủ', 'Cầu thủ trẻ nhất', 'Vị trí của cầu thủ > 25 tuổi')
+        self.qaOpt = ('Tìm tên cầu thủ', 'Cầu thủ trẻ nhất', 'Vị trí của cầu thủ > 25 tuổi')
         self.saveOpt = ('Lưu Biểu Đồ', 'Lưu Dữ Liệu')
         self.pos.sort()
         self.club.sort()
         self.df = None
+        if 'ssDf' not in st.session_state:
+            st.session_state.ssDf = self.df
         self.home()
 
     def home(self):
@@ -68,26 +70,25 @@ class main:
 
         if choice == self.menu[0]:  # Trang chủ
             st.title('Trang chủ')
-            st.image("football-manager-2021.jpg")
-            buttonOpenFile = st.file_uploader("Tải file dữ liệu lên", type=["db", "csv", "xlsx"])
-            if buttonOpenFile is not None:
-                st.info('Dữ liệu được thêm hoàn tất')
-                if st.checkbox("Bật tắt hiển thị dữ liệu"):
+            st.image("football-manager-champion.jpg")
+            buttonOpenFile = st.file_uploader("Tải file dữ liệu", type=["db", "csv", "xlsx"])
+            if st.checkbox("Bật tắt hiển thị dữ liệu"):
+                if buttonOpenFile is not None:
+                    st.info('Dữ liệu được thêm hoàn tất')
                     _, fileExtension = os.path.splitext(str(buttonOpenFile.name))
                     if fileExtension in ['.xlsx', '.xls']:
                         self.df = pd.read_excel(str(buttonOpenFile.name), engine='openpyxl')
-                        if 'ssDf' not in st.session_state:
-                            st.session_state.ssDf = self.df
-                        st.dataframe(st.session_state.ssDf)
                     elif fileExtension in ['.csv']:
                         self.df = pd.read_csv(str(buttonOpenFile.name), encoding='utf-8')
-                        if 'ssDf' not in st.session_state:
-                            st.session_state.ssDf = self.df
-                        st.dataframe(st.session_state.ssDf)
                     else:  # for *.db file
-                        self.displayDb(str(buttonOpenFile.name))
-            else:
-                st.warning('File dữ liệu chưa được thêm')
+                        conn = self.displayDb(str(buttonOpenFile.name))
+                        self.df = pd.DataFrame(get_data(conn))
+                    print('ssDf' not in st.session_state)
+                elif 'ssDf' in st.session_state:
+                    st.info('File dữ liệu đã được lưu trong bộ nhớ tạm')
+                else:
+                    st.info('Dữ liệu chưa được thêm')
+                self.session_state_df(self.df)
 
             nameValue = st.text_input("Full Name", help='Nhập họ và tên cầu thủ')
             col1, col2 = st.columns(2)
@@ -122,22 +123,77 @@ class main:
             if buttonSave:
                 st.markdown(download_link(st.session_state.ssDf), unsafe_allow_html=True)
         elif choice == self.menu[1]:  # Hỏi đáp
+            import re
+            _newDf = None
+            _copy_Df = None
+            split_space_word = []
             st.title('Hỏi Đáp')
-            if self.df is None:
-                st.warning('Chưa nhập bất cứ dữ liệu nào')
-            else:
-                st.info('Dữ liệu đã được nhập')
-                st.dataframe(st.session_state.ssDf)
-                boxQa = st.selectbox('Lựa Chọn Câu Hỏi', options=self.qaOpt)
-                buttonQa = st.button('Trả Lời')
+            if st.session_state.ssDf is not None:
+                _copy_Df = cp.deepcopy(st.session_state.ssDf)
+                st.dataframe(_copy_Df)
+                col1, col2 = st.columns([4, 1])
+                clearDB = col2.button('Clear dữ liệu')
+                if clearDB:
+                    _copy_Df = _copy_Df[0:0]
+                    col1.warning('Dữ liệu chưa được nhập')
+                else:
+                    col1.info('Dữ liệu đã được nhập')
+            boxQa = st.selectbox('Lựa Chọn Câu Hỏi', options=self.qaOpt)
 
-                if buttonQa:
-                    if boxQa == self.qaOpt[0]:
-                        pass
-                    elif boxQa == self.qaOpt[1]:
-                        pass
-                    elif boxQa == self.qaOpt[2]:
-                        pass
+            if boxQa == self.qaOpt[0]:
+                names = st.text_input('Nhập tên cầu thủ muốn tìm')  # Người dùng nhập 1 hoặc nhiều tên
+                                                                    # và cách nhau bằng dấu phẩy
+                optionSearch = st.radio('Cách tìm kiếm', ('Chính Xác', 'Tương Đối'), index=0)
+                if st.session_state.ssDf is not None:
+                    split_comma = names.split(',')
+                    for word in split_comma:
+                        word_list = re.findall(r"[\w']+", word)
+                        split_space_word.extend(word_list)
+                    _newDf = self.search_string(split_space_word, optionSearch)
+
+            elif boxQa == self.qaOpt[1]:
+                pass
+                # tuoiBox, cancel = self.dialogBirthBox()
+                # if not cancel:
+                #     return None
+                #
+                # result = []
+                # today = date.today()
+                # yearCurrent = today.strftime('%Y')
+                # tuoiDf = cp.deepcopy(self.df)
+                #
+                # for dateIdx in tuoiDf['Ngày Sinh']:
+                #     date_transfer_df = datetime.strptime(dateIdx, '%d/%m/%Y')
+                #     year_df = date_transfer_df.strftime('%Y')
+                #     tuoiPlayer = int(yearCurrent) - int(year_df)
+                #     if tuoiPlayer > tuoiBox:
+                #         result.append(True)
+                #     else:
+                #         result.append(False)
+                # tuoiDf['result'] = result
+                # tuoiDf = tuoiDf[tuoiDf['result'] == True]
+                # obj = tuoiDf.drop(columns='result')
+                #
+                # return obj
+            elif boxQa == self.qaOpt[2]:
+                pass
+                # posTeamDf = cp.deepcopy(self.df)
+                # self.dialog = inputPosClub(option=2)
+                # if self.dialog.exec_():
+                #     pos, club = self.dialog.getInputs()
+                #     posTeamDf = posTeamDf[posTeamDf['Vị Trí'] == pos]
+                #     posTeamDf = posTeamDf[posTeamDf['Câu Lạc Bộ'] == club]
+                # else:
+                #     return None
+                # obj = posTeamDf
+                # return obj
+
+            buttonQa = st.button('Trả Lời')
+            if buttonQa:
+                if _newDf is not None:
+                    st.dataframe(_newDf)
+                else:
+                    st.error('Không có dữ liệu để trả lời câu hỏi')
         elif choice == self.menu[2]:  # Biểu đồ
             st.title('Biểu Đồ')
             if self.df is None:
@@ -152,10 +208,24 @@ class main:
             st.title('Liên Hệ')
             self.info()
 
+    def session_state_df(self, dataframe):
+        if dataframe is not None:
+            st.session_state.ssDf = dataframe
+        return st.dataframe(st.session_state.ssDf)
+
+    def search_string(self, word_list_name, option=0):
+        cp_df = cp.deepcopy(st.session_state.ssDf)
+        if option == 'Chính Xác':
+            obj = cp_df[np.logical_and.reduce([cp_df['Họ và Tên'].str.contains(word) for word in word_list_name])]
+        else:
+            obj = cp_df[cp_df['Họ và Tên'].str.contains('|'.join(word_list_name))]
+        return obj
+
     def graph(self):
         pass
 
-    def info(self):
+    @staticmethod
+    def info():
         st.subheader('FOOTBALL MANAGER\n')
         st.code("Phần mềm quản lý cầu thủ đang trong giai đoạn thử nghiệm\n"
                 "---------------------------------------------"
@@ -166,24 +236,20 @@ class main:
                 "\n           Trần Huỳnh Quốc Vũ"
                 "\n      SĐT: 0944 886 896")
 
-    def importTable(self, lst):
+    @staticmethod
+    def importTable(lst):
         # Thêm vào database table
         st.session_state.ssDf.loc[-1] = lst
         st.session_state.ssDf.index += 1
         st.session_state.ssDf.sort_index(inplace=True)
 
-    def displayDb(self, path=None):
+    @staticmethod
+    def displayDb(path=None):
         if path is None:
             return
         conn = get_connection(path)
         init_db(conn)
-        self.display_data(conn)
-
-    def display_data(self, conn: Connection):
-        st.dataframe(get_data(conn))
-        self.df = pd.DataFrame(get_data(conn))
-        if 'ssDf' not in st.session_state:
-            st.session_state.ssDf = self.df
+        return conn
 
 
 if __name__ == '__main__':
